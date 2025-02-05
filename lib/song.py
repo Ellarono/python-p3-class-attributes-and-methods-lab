8b04dc75eb7e7b1b59c4b8fc85efeb8aa745712b
Daniella Rono <daniellarono@gmail.com>
2025-02-05 23:05:03 +0300

    all tests are passing

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,26 @@
 class Song:
-    pass
+    # Class attributes
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        # Instance attributes
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+
+        # Update class attributes
+        Song.count += 1
+
+        # Update genres list and genre_count dictionary
+        if genre not in Song.genres:
+            Song.genres.append(genre)
+        Song.genre_count[genre] = Song.genre_count.get(genre, 0) + 1
+
+        # Update artists list and artist_count dictionary
+        if artist not in Song.artists:
+            Song.artists.append(artist)
+        Song.artist_count[artist] = Song.artist_count.get(artist, 0) + 1
